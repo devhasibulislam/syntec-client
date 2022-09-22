cde863a234d0d48b05b20b420bcd9fe1bc7ef2de
hasibulislam999 <hasib143sl@gmail.com>
2022-09-23 00:59:13 +0600

    not found page created

---
+++ src/pages/notFound.js
@@ -0,0 +1,11 @@
+import React from "react";
+const NotFound = () => {
+  return (
+    <section className="vh-100 vw-100 d-flex justify-content-center align-items-center">
+      <h1 className="mb-0">
+        <b>404</b> | Not Found...
+      </h1>
+    </section>
+  );
+};
+export default NotFound;
