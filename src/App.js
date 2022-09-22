6e1bf8d3a18fa80842cc9fb37569ae75f19b55a6
hasibulislam999 <hasib143sl@gmail.com>
2022-09-23 00:59:26 +0600

    driver modified

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { Home } from "./pages";
 import SigninPage from "./pages/signin";
+import SignupPage from "./pages/signup";
+import NotFound from "./pages/notFound";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Switch>
         <Route path="/" component={Home} exact />
         <Route path="/signin" component={SigninPage} exact />
+        <Route path="/signup" component={SignupPage} exact />
+        <Route path="*" component={NotFound} exact />
       </Switch>
     </Router>
   );
