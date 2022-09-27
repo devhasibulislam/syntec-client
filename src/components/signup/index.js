d486bfff1a263af40ec449d6f05fbdf048e611bf
hasibulislam999 <hasib143sl@gmail.com>
2022-09-28 01:00:57 +0600

    sign up segment complete

--- src/components/signup/index.js
+++ src/components/signup/index.js
@@ -1,44 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   SignupContainer,
   SignupWrapper,
   SignupP,
   SignupInput,
 } from "./SignupElements";
-import { Button } from "../ButtonElements";
 
 const Signup = () => {
+  const [avatar, setAvatar] = useState("");
+
+  // upload avatar to server
+  function handleAvatarUpload(event) {
+    const formData = new FormData();
+    formData.append("avatar", event.target.files[0]);
+
+    const postUserAvatar = async () => {
+      const request = await fetch(`http://localhost:8080/avatar/`, {
+        method: "POST",
+        body: formData,
+      });
+      const response = await request.json();
+      setAvatar(response.data);
+      if (response.success) {
+        // return toast.success("avatar uploaded successfully.");
+        console.log("Avatar uploaded successfully.");
+      }
+    };
+    postUserAvatar();
+  }
+
+  // upload user info to server
+  function handleRegistration(event) {
+    event.preventDefault();
+
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    const userInfo = { name, email, password, avatar };
+    // console.log(userInfo);
+
+    const postAnUser = async () => {
+      const request = await fetch(`http://localhost:8080/user`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(userInfo),
+      });
+      const response = await request.json();
+      if (response.acknowledgement) console.log(response);
+    };
+    postAnUser();
+
+    event.target.reset();
+  }
+
   return (
     <SignupContainer>
       <SignupWrapper className="py-5 px-5">
         <div className="my-auto">
           <h3 className="center-align mb-5">Sign Up to your Account</h3>
-          <form>
+          <form onSubmit={handleRegistration}>
             <SignupP>Name</SignupP>
             <SignupInput
-              name="name"
               type="text"
+              name="name"
               placeholder="Enter your name..."
             />
             <SignupP>Email</SignupP>
             <SignupInput
+              type="email"
               name="email"
-              type="text"
               placeholder="Enter your email..."
             />
             <SignupP>Password</SignupP>
             <SignupInput
-              name="password"
               type="password"
+              name="password"
               placeholder="Enter your password..."
             />
             <div className="mb-3">
               <label for="formFile" className="form-label">
                 <b>Upload your avatar</b>
               </label>
-              <input className="form-control" type="file" id="formFile" />
+              <input
+                className="form-control"
+                type="file"
+                name="avatar"
+                id="formFile"
+                accept=".png, .jpg, jpeg"
+                onChange={handleAvatarUpload}
+              />
             </div>
-            <Button className="mt-5 mb-3">Continue</Button>
+            <button type="submit" className="my-3 px-5 btn btn-outline-dark">
+              Continue
+            </button>
           </form>
         </div>
       </SignupWrapper>
