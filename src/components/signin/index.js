8b794137af9216da2a9351732b37957c6a078ea9
hasibulislam999 <hasib143sl@gmail.com>
2022-09-29 13:18:13 +0600

    sign in index modified

--- src/components/signin/index.js
+++ src/components/signin/index.js
@@ -5,24 +5,55 @@ import {
   SigninP,
   SigninInput,
 } from "./SigninElements";
-import { Button } from "../ButtonElements";
 
 const Signin = () => {
+  function handleLogin(event) {
+    event.preventDefault();
+
+    const userInfo = {
+      email: event.target.email.value,
+      password: event.target.password.value,
+    };
+    // console.log(userInfo);
+
+    const postAnUser = async () => {
+      const request = await fetch(`http://localhost:8080/user/signin`, {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(userInfo),
+      });
+      const response = await request.json();
+      if (response.acknowledgement) console.log(response);
+    };
+    postAnUser();
+
+    event.target.reset();
+  }
+
   return (
     <SigninContainer>
       <SigninWrapper className="py-5 px-5">
         <div className="my-auto">
           <h3 className="center-align mb-5">Sign In to your Account</h3>
-          <form>
+          <form onSubmit={handleLogin}>
             <SigninP>Email</SigninP>
-            <SigninInput type="text" placeholder="Enter your email..." />
+            <SigninInput
+              type="text"
+              name="email"
+              placeholder="Enter your email..."
+            />
             <SigninP>Password</SigninP>
-            <SigninInput type="password" placeholder="Enter your password..." />
-            <Button className="mt-5 mb-3">Continue</Button>
+            <SigninInput
+              type="password"
+              name="password"
+              placeholder="Enter your password..."
+            />
+            <button type="submit" className="mt-3 px-5 btn btn-outline-dark">
+              Continue
+            </button>
           </form>
-          <p className="center-align">
-            Forgot your password? <a href="/">Reset here</a>
-          </p>
         </div>
       </SigninWrapper>
     </SigninContainer>
